5623fa30cb6af416147c914b4bc0bfed2650b739
juarezadolfo <juarezadolfo@hotmail.com>
2018-01-12 12:41:42 -0800

    final commit - sort of works now

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Adolfo Juarez',
+      profilePic: 'https://media.sandiegoreader.com/img/photos/2017/11/14/Adolfo-Juarez-and-family_t670.jpg?b3f6a5d7692ccc373d56e40cf708e3fa67d9af9d',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
